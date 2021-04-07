6535cf2b7eadf3dc58c3903e5b59415fde782e8e
Mandella-thi <thiagogarc@gmail.com>
2021-04-07 10:16:23 -0300

    "arrayOf string"

---
+++ src/main/kotlin/one/digitalInovation/Colection/TesteArrayString.kt
@@ -0,0 +1,19 @@
+package one.digitalInovation.Colection
+
+fun main() {
+    val nomes =Array(3){""}
+
+    nomes[0]="Maria"
+    nomes[1]= "Zaza"
+    nomes[2]="José"
+    for (nome in nomes){
+        println(nome)
+    }
+    println("=========================")
+    nomes.sort()
+    nomes.forEach { println(it) }
+println("================================")
+    val nomes2= arrayOf("Maria", "zaza","pedro")
+    nomes2.sort()
+    nomes2.forEach { println(it) }
+}
