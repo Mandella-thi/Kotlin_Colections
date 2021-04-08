3f98c3578985ed093b444e95e4833ea1488dfc18
Mandella-thi <thiagogarc@gmail.com>
2021-04-08 08:49:42 -0300

    sortedBy e groupedBy

--- src/main/kotlin/one/digitalInovation/Colection/TesteList.kt
+++ src/main/kotlin/one/digitalInovation/Colection/TesteList.kt
@@ -1,18 +1,26 @@
 package one.digitalInovation.Colection
 
 fun main() {
-    val joao= Funcionario (nome = "joão", salario = 1000.0)
-    val pedro= Funcionario (nome = "pedro", salario = 1000.0)
-    val maria= Funcionario (nome = "maria", salario = 1000.0)
+    val joao= Funcionario (nome = "joão", salario = 2000.0, tipoContratacao = "CLT")
+    val pedro= Funcionario (nome = "pedro", salario = 1500.0, tipoContratacao = "PJ")
+    val maria= Funcionario (nome = "maria", salario = 4000.0, tipoContratacao = "CLT")
 
     val funcionarios = listOf(joao, pedro, maria)
     funcionarios.forEach{println(it)}
     println("==============================")
     println(funcionarios.find { it.nome== "maria" })
+    println("================================")
+    funcionarios.sortedBy { it.salario }.forEach { println(it) }
+    println("================================")
+    funcionarios
+        .groupBy { it.tipoContratacao }
+        .forEach { println(it) }
+
 }
 data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContratacao: String
 ){
 override fun toString(): String=
     """
