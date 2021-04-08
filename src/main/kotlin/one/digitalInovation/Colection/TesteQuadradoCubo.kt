e96d392dd2cd751d7f077811387c6d05511cb948
Mandella-thi <thiagogarc@gmail.com>
2021-04-08 15:43:56 -0300

    quadrado cubo

---
+++ src/main/kotlin/one/digitalInovation/Colection/TesteQuadradoCubo.kt
@@ -0,0 +1,12 @@
+package one.digitalInovation.Colection
+
+fun main() {
+    //insira as variaveis corretamente
+    val r = readLine()
+    for (i in 1..r!!.toInt()) {
+
+        println("$i ${i  * i  } ${i  *i  *i  }")
+
+
+    }
+}
